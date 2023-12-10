8aecb2d73f253875a138e05e56b8c0d6217a3d33
unknown <pjrbusiness@outlook.com>
2023-12-10 16:06:16 +0000

    Added JS to make element editable and to add to local storage

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,23 @@ function myTimer() {
   let time = dayjs().format("HH:mm:ss");
   $("#currentDay").text(day + ", " + date);
 }
+
+/*Script for task input*/
+//select inputElement
+const inputElement = document.querySelector("#input-element-1");
+
+//Keep saved content visible on page from local storage
+const savedContent = localStorage.getItem("input-1");
+if (savedContent) {
+  inputElement.innerText = savedContent;
+}
+
+//Save content to local storage
+function saveToLocalStorage() {
+  let updatedContent = inputElement.innerText;
+  console.log("Your 8am Task is: " + updatedContent);
+
+  localStorage.setItem("input-1", updatedContent);
+}
+
+$("#save-column-1 .saveBtn").on("click", saveToLocalStorage);
