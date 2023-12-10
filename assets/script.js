a3451d89332174630f31279c89dd273407d2d8e6
unknown <pjrbusiness@outlook.com>
2023-12-10 15:04:23 +0000

    Added additional classes and some basic CSS styling

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
+/* Script for the day/date timer*/
 setInterval(myTimer, 1000);
 
 function myTimer() {
   let day = dayjs().format("dddd");
   let date = dayjs().format("DD MMM YYYY");
+  let time = dayjs().format("HH:mm:ss");
   $("#currentDay").text(day + ", " + date);
 }
